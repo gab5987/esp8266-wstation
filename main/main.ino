2b72ecae32e1cea8e61100a17f7d41146815cb8d
Gab Novalsky <99911394+gab5987@users.noreply.github.com>
2022-03-28 20:15:02 -0300

    Add files via upload

---
+++ main/main.ino
@@ -0,0 +1,31 @@
+#define debug
+// Preparação do sensor DHT11
+#include "DHT.h"
+#define DHTPIN 4 //pino digital conectado ao DHT11 (gpio4 - d2)
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+float umidade = NULL;
+float temperatura = NULL;
+float sensacao_termica = NULL;
+
+void le_umidade_temperatura();
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  delay(1000);
+  le_umidade_temperatura();
+
+  #ifndef teste
+  Serial.print(umidade);
+  Serial.print(" ");
+  Serial.print(temperatura);
+  Serial.print(" ");
+  Serial.print(sensacao_termica);
+  Serial.println();
+  #endif
+}
