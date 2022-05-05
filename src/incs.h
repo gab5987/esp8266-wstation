89d77f724c704e91c021733a5f8104f29382dce2
gab5987 <biell.gb4@gmail.com>
2022-05-04 21:30:29 -0300

    few changes to optimize

--- src/incs.h
+++ src/incs.h
@@ -1,6 +1,8 @@
 #include <Arduino.h>
-void read_humidity_temperature();
+void read_humidity_temperature(float *hu, float *tempC, float *tempF, float *temp_index);
 void send_thingspeak(String string_data);
 void init_wifi(void);
 void conect_wifi(void);
 void verify_wifi(void);
+void read_air_pressure();
+int read_wind_speed();
